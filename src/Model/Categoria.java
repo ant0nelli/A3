d32c45b15dd3885aa56f4879b56943cde59256b9
ant0nelli <arthur.antonelli76@gmail.com>
2025-05-18 18:20:45 -0300

    criacao das classes categoria  e produto no pacode model

---
+++ src/Model/Categoria.java
@@ -0,0 +1,50 @@
+package Model;
+
+public class Categoria {
+    private int id;
+    private String nome;
+    private String tamanho;
+    private String embalagem;
+
+    // Construtor vazio
+    public Categoria() {
+
+    }
+
+    // Construtor com parametro
+    public Categoria(int id, String nome, String tamanho, String embalagem) {
+        this.id = id;
+        this.nome = nome;
+        this.tamanho = tamanho;
+        this.embalagem = embalagem;
+    }
+
+    // Get e setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getEmbalagem() {
+        return embalagem;
+    }
+    public void setEmbalagem(String embalagem) {
+        this.embalagem = embalagem;
+    }
+}
---
+++ src/Model/Produto.java
@@ -0,0 +1,92 @@
+package Model;
+
+public class Produto {
+    private int id;
+    private String nome;
+    private double preco;
+    private String unidade;
+    private int quantidade_estoque;
+    private int quantidade_min_estoque;
+    private int quantidade_max_estoque;
+    private String categoria;
+
+    //Construtor vazio
+    public Produto() {
+
+    }
+    //Construtor com prarametro
+    public Produto(int id, String nome, double preco, String unidade,int quantidade_estoque, int quantidade_min_estoque, int quantidade_max_estoque, String categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.unidade = unidade;
+        this.quantidade_estoque = quantidade_estoque;
+        this.quantidade_min_estoque = quantidade_min_estoque;
+        this.quantidade_max_estoque = quantidade_max_estoque;
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidade_estoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidade_estoque = quantidadeEstoque;
+    }
+
+    public int getQuantidadeMinEstoque() {
+        return quantidade_min_estoque;
+    }
+
+    public void setQuantidadeMinEstoque(int quantidadeMinEstoque) {
+        this.quantidade_min_estoque = quantidadeMinEstoque;
+    }
+
+    public int getQuantidadeMaxEstoque() {
+        return quantidade_max_estoque;
+    }
+
+    public void setQuantidadeMaxEstoque(int quantidadeMaxEstoque) {
+        this.quantidade_max_estoque = quantidadeMaxEstoque;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
