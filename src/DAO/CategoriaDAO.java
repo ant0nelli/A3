52be22ebdba01631c092a0ec43316dc436f1a7e9
ant0nelli <arthur.antonelli76@gmail.com>
2025-05-19 20:51:59 -0300

    add:CategoriaDAO.java

---
+++ src/DAO/CategoriaDAO.java
@@ -0,0 +1,106 @@
+package DAO;
+import Model.Categoria;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class CategoriaDAO{
+    //Conexão com MYSQL
+    public Connection conectar(){
+        try {
+            String url = "jdbc:mysql://localhost:3306/db_estoque";
+            String user = "root";
+            String password = "root123";
+            return DriverManager.getConnection(url,user,password);
+        } catch (SQLException e) {
+            System.out.println("Não conectou");
+            return null;
+        }
+    }
+    //Criação
+    public boolean insertCategoria (Categoria categoria){
+    String sql = "INSERT INTO tb_categorias (id, nome, tamanho, embalagem) VALUES (?, ?, ?, ?)";
+    try (Connection conn = conectar();
+         PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+        stmt.setInt(1, categoria.getId());
+        stmt.setString(2, categoria.getNome());
+        stmt.setString(3, categoria.getTamanho());
+        stmt.setString(4, categoria.getEmbalagem());
+
+        stmt.executeUpdate();  
+        stmt.close();
+
+        return true;
+
+    } catch (SQLException e) {
+        System.out.println("Erro:" + e);
+        throw new RuntimeException(e);
+    }
+}
+
+    //Ler
+    public ArrayList<Categoria> ListaCategorias = new ArrayList<>();
+    public ArrayList<Categoria> getListaCategorias(){
+        ListaCategorias.clear();
+        String sql = "SELECT * FROM tb_categorias";
+        try(Connection conn = conectar();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)){
+                while (rs.next()){
+                    int id = rs.getInt("id");
+                    String nome = rs.getString("nome");
+                    String tamanho = rs.getString("tamanho");
+                    String embalagem = rs.getString("embalagem");
+
+                    Categoria categoria = new Categoria (id, nome, tamanho, embalagem);
+
+                    ListaCategorias.add(categoria);
+                }
+                stmt.close();
+            }catch (SQLException e){
+                System.out.println("Erro:" + e);
+            }
+        return ListaCategorias;
+    }
+    public void setListaCategorias(ArrayList<Categoria> ListaCategorias){
+        this.ListaCategorias = ListaCategorias;
+    }
+
+    //Update
+    public boolean updateCategoria(Categoria categoria){
+        String sql = "UPDATE tb_categorias set nome = ?, tamanho = ?, embalagem = ? WHERE id = ?";
+        try (Connection conn = conectar();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+            
+            stmt.setString(1, categoria.getNome());
+            stmt.setString(2, categoria.getTamanho());
+            stmt.setString(3, categoria.getEmbalagem());
+            stmt.setInt(4, categoria.getId());
+
+            stmt.execute();
+            stmt.close();
+            return true;
+            
+        } catch (SQLException e) {
+            System.out.println("Erro:" + e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    //Remove
+    public boolean deleteCategoria(Categoria categoria){
+        String sql = ("DELETE from tb_categorias WHERE id = ? ");
+        try (Connection conn = conectar();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+                
+                stmt.setInt(1, categoria.getId());
+                stmt.executeUpdate();
+                return true;
+
+            } catch (SQLException e){
+                System.out.println("Erro" + e);
+                throw new RuntimeException(e);
+            }
+    }
+
+}
