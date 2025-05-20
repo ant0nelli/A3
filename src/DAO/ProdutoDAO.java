27c9431865a3fccfe2e38d580c43b6b0220265bc
notbaccin <b4cc1n@gmail.com>
2025-05-19 22:44:27 -0300

    add: Criação do Produto DAO

---
+++ src/DAO/ProdutoDAO.java
@@ -0,0 +1,92 @@
+package DAO;
+
+import Model.Produto;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoDAO {
+    private Connection connection;
+
+    public ProdutoDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void inserir(Produto produto) throws SQLException {
+        String sql = "INSERT INTO produtos (nome, preco, unidade, quantidade_estoque, quantidade_min_estoque, quantidade_max_estoque, categoria) " +
+                     "VALUES (?, ?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, produto.getNome());
+            stmt.setDouble(2, produto.getPreco());
+            stmt.setString(3, produto.getUnidade());
+            stmt.setInt(4, produto.getQuantidadeEstoque());
+            stmt.setInt(5, produto.getQuantidadeMinEstoque());
+            stmt.setInt(6, produto.getQuantidadeMaxEstoque());
+            stmt.setString(7, produto.getCategoria());
+            stmt.executeUpdate();
+        }
+    }
+
+    public void atualizar(Produto produto) throws SQLException {
+        String sql = "UPDATE produtos SET nome = ?, preco = ?, unidade = ?, quantidade_estoque = ?, quantidade_min_estoque = ?, quantidade_max_estoque = ?, categoria = ? " +
+                     "WHERE id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, produto.getNome());
+            stmt.setDouble(2, produto.getPreco());
+            stmt.setString(3, produto.getUnidade());
+            stmt.setInt(4, produto.getQuantidadeEstoque());
+            stmt.setInt(5, produto.getQuantidadeMinEstoque());
+            stmt.setInt(6, produto.getQuantidadeMaxEstoque());
+            stmt.setString(7, produto.getCategoria());
+            stmt.setInt(8, produto.getId());
+            stmt.executeUpdate();
+        }
+    }
+
+    public void deletar(int id) throws SQLException {
+        String sql = "DELETE FROM produtos WHERE id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        }
+    }
+
+    public Produto buscarPorId(int id) throws SQLException {
+        String sql = "SELECT * FROM produtos WHERE id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return mapearProduto(rs);
+                }
+            }
+        }
+        return null;
+    }
+
+    public List<Produto> listarTodos() throws SQLException {
+        List<Produto> produtos = new ArrayList<>();
+        String sql = "SELECT * FROM produtos";
+        try (PreparedStatement stmt = connection.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+            while (rs.next()) {
+                produtos.add(mapearProduto(rs));
+            }
+        }
+        return produtos;
+    }
+
+    private Produto mapearProduto(ResultSet rs) throws SQLException {
+        Produto produto = new Produto();
+        produto.setId(rs.getInt("id"));
+        produto.setNome(rs.getString("nome"));
+        produto.setPreco(rs.getDouble("preco"));
+        produto.setUnidade(rs.getString("unidade"));
+        produto.setQuantidadeEstoque(rs.getInt("quantidade_estoque"));
+        produto.setQuantidadeMinEstoque(rs.getInt("quantidade_min_estoque"));
+        produto.setQuantidadeMaxEstoque(rs.getInt("quantidade_max_estoque"));
+        produto.setCategoria(rs.getString("categoria"));
+        return produto;
+    }
+}
