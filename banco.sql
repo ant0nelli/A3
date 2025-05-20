b7a9fb3080de8f932a293c8ebf3abaa4214f8619
ant0nelli <arthur.antonelli76@gmail.com>
2025-05-19 22:08:14 -0300

    feat: adiciona primary key e foreign key para a tb_produtos e tb_categorias

--- banco.sql
+++ banco.sql
@@ -1,16 +1,21 @@
 create database if not exists db_estoque;
 use db_estoque;
 create table tb_categorias(
+    id_categoria int not null,
     nome varchar(100),
     tamanho varchar(20),
-    embalagem varchar(50)
+    embalagem varchar(50),
+    primary key(id_categoria)
 );
 create table tb_produtos(
+    id_produto int not null,
     nome varchar(100),
     preco decimal(10,2),
-    unidade int,
+    unidade varchar(20),
     quantidade_estoque int,
     quantidade_min_estoque int,
     quantidade_max_estoque int,
-    categoria varchar(100)
+    primary key(id_produto),
+    id_categoria int not null,
+    constraint fk_id_categoria foreign key (id_categoria) references tb_categorias(id_categoria)
 );
